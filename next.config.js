6d7d892256d6942f7384dbc9547f63036a3dd128
Мария Лось <marialos@Air-Maria.Dlink>
2022-03-10 09:38:28 +0300

    fixed config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['ru-RU', 'en-US'],
+    defaultLocale: 'ru-RU',
+    localeDetection: false,
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
